e43cbbf7fd739837f37415fbd588e590a3c4475e
Mahesh Darkunde <mahesh@Maheshs-MacBook-Air.local>
2025-06-29 20:33:43 +0530

    live commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['randomuser.me', 'localhost'],
+    domains: ['randomuser.me', 'localhost', 'mapacultural.gestorcultural.com.br'],
   },
 }
 
